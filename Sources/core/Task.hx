7330420c5bb06e6a0aa913f38fee58741ce94ff5
Aaditya Singh <aaditya.singh.met16@itbhu.ac.in>
2021-03-07 12:45:09 +0530

    Added color to Task.

--- Sources/core/Task.hx
+++ Sources/core/Task.hx
@@ -10,12 +10,19 @@ class Task {
     public var overlaps : Bool;
     public var order : Null<Int>;
     public var finished : Bool;
+    public var r : Null<Int>;
+    public var g : Null<Int>;
+    public var b : Null<Int>;
+
     // shiftable
-    public function new(start, duration, title, overlaps, finished=false) {
+    public function new(start, duration, title, overlaps, finished=false, ?r, ?g, ?b) {
         this.start = start;
         this.duration = duration;
         this.title = title;
         this.overlaps = overlaps;
         this.finished = finished;
+        this.r = r;
+        this.g = g;
+        this.b = b;
     }
 }
